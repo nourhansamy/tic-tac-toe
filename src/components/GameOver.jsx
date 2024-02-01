c12b83e9482baa1be242bd4407b86cefb3729014
Nourhan <nourhan.el-shenawy@eg.ibm.com>
2024-02-01 16:03:26 +0200

    Small change

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -4,7 +4,7 @@ function GameOver({ winnerName, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
-      {winnerName ? <p>{winnerName} won!</p> : <p>It's a draw!</p>}
+      {winnerName ? <p>{winnerName} won!!!!</p> : <p>It's a draw!</p>}
       <p>
         <button onClick={onRestart}>Rematch!</button>
       </p>
